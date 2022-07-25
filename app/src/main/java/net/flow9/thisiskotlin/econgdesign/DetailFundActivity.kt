00d324a272027d40a13b25a52f2a0df6271f3c3a
jejxis <3190021@gmail.com>
2022-07-25 21:54:46 +0900

    init

---
+++ app/src/main/java/net/flow9/thisiskotlin/econgdesign/DetailFundActivity.kt
@@ -0,0 +1,11 @@
+package net.flow9.thisiskotlin.econgdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailFundActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailF)
+    }
+}
---
+++ app/src/main/java/net/flow9/thisiskotlin/econgdesign/DetailPurActivity.kt
@@ -0,0 +1,11 @@
+package net.flow9.thisiskotlin.econgdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailPurActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_pur)
+    }
+}
