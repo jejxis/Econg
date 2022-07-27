52694eafdfe0caf4ff21f589b9e1d6be14eba523
jejxis <3190021@gmail.com>
2022-07-28 03:44:09 +0900

    init

--- app/src/main/java/net/flow9/thisiskotlin/econgdesign/HomeActivity.kt
+++ app/src/main/java/net/flow9/thisiskotlin/econgdesign/HomeActivity.kt
@@ -1,11 +1,15 @@
 package net.flow9.thisiskotlin.econgdesign
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import net.flow9.thisiskotlin.econgdesign.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    val binding by lazy {ActivityHomeBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(binding.root)
     }
+
 }
