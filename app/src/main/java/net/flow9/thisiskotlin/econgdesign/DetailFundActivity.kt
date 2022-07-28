4fd86e1bf865fd8d4fa0f47635e359ddb75ad7b5
jejxis <3190021@gmail.com>
2022-07-29 04:12:45 +0900

    menu header

--- app/src/main/java/net/flow9/thisiskotlin/econgdesign/DetailFundActivity.kt
+++ app/src/main/java/net/flow9/thisiskotlin/econgdesign/DetailFundActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class DetailFundActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detailF)
+        setContentView(R.layout.activity_detail_fund)
     }
 }
--- app/src/main/java/net/flow9/thisiskotlin/econgdesign/HomeActivity.kt
+++ app/src/main/java/net/flow9/thisiskotlin/econgdesign/HomeActivity.kt
@@ -2,14 +2,38 @@ package net.flow9.thisiskotlin.econgdesign
 
 import android.R
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
 import net.flow9.thisiskotlin.econgdesign.databinding.ActivityHomeBinding
+import net.flow9.thisiskotlin.econgdesign.databinding.MenuHeaderBinding
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     val binding by lazy {ActivityHomeBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.btnMenu.setOnClickListener{
+            binding.layoutDrawer.openDrawer(GravityCompat.START)//START : left, END: right
+        }
+
+        binding.naviView.setNavigationItemSelectedListener(this)//내비게이션 메뉴 아이템에 클릭 속성 부여
+
+    }
+
+    override fun onBackPressed() {
+        if(binding.layoutDrawer.isDrawerOpen(GravityCompat.START)){//내비게이션 열려있으면 실행
+            binding.layoutDrawer.closeDrawers()
+        }
+
+        else super.onBackPressed()//일반 백 버튼 기능 실행(finish)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 
 }
