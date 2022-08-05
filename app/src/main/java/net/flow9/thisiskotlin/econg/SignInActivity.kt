595846857e62f2b8b846fc3bbc6e56706d19e8e7
jejxis <3190021@gmail.com>
2022-08-05 14:25:47 +0900

    branchtest

--- app/src/main/java/net/flow9/thisiskotlin/econg/SignInActivity.kt
+++ app/src/main/java/net/flow9/thisiskotlin/econg/SignInActivity.kt
@@ -23,7 +23,6 @@ class SignInActivity : AppCompatActivity() {
         binding.btnGoToSU.setOnClickListener {
             val intent = Intent(this@SignInActivity, SignUpActivity::class.java)
             startActivity(intent)
-            finish()
         }
     }
 }
