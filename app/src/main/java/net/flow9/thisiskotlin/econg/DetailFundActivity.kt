5e2c1a998910ae0d6fc2d9358e9e7ebd12eb5d4d
siss-ankisile <ankisile@gmail.com>
2022-08-08 00:53:35 +0900

    [REVISE] DetailPurActivity, DetailFundActivity

--- app/src/main/java/net/flow9/thisiskotlin/econg/DetailFundActivity.kt
+++ app/src/main/java/net/flow9/thisiskotlin/econg/DetailFundActivity.kt
@@ -2,19 +2,50 @@ package net.flow9.thisiskotlin.econg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import net.flow9.thisiskotlin.econg.R
+import android.util.Log
+import net.flow9.thisiskotlin.econg.data.GetProductDetail
 import net.flow9.thisiskotlin.econg.databinding.ActivityDetailFundBinding
-import net.flow9.thisiskotlin.econg.databinding.ActivityHomeBinding
+import net.flow9.thisiskotlin.econg.interfaceModel.APIS
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DetailFundActivity : AppCompatActivity() {
 
     val binding by lazy { ActivityDetailFundBinding.inflate(layoutInflater)}
+    val api = APIS.create()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         if(intent.hasExtra("id")){
-            binding.productName.text = intent.getStringExtra("id")
+            val id = intent.getStringExtra("id")
+            if (id != null) {
+                api.get_product_detail(id)
+                    .enqueue(object : Callback<GetProductDetail> {
+                        override fun onResponse(
+                            call: Call<GetProductDetail>,
+                            response: Response<GetProductDetail>
+                        ) {
+                            Log.d("log", response.toString())
+                            Log.d("log", response.body().toString())
+                            binding.productName.text = response.body()?.title.toString()
+                            binding.fundDead.text = response.body()?.deadline.toString()
+                            binding.fundStat.text = response.body()?.price.toString()
+                            binding.comInfo.text = response.body()?.companyName.toString()
+                            binding.productInfo.text = response.body()?.explanation.toString()
+                        }
+
+                        override fun onFailure(call: Call<GetProductDetail>, t: Throwable) {
+                            // 실패
+                            Log.d("log", t.message.toString())
+                            Log.d("log", "fail")
+                        }
+
+                    })
+            }
+
+
         }
         if(intent.hasExtra("info")){
             binding.fundStat.text = intent.getStringExtra("info")
